c5de89c5ecb7a43dbc54d3f9eef3260113343bc5
User <verazmushko@gmail.com>
2018-10-02 13:18:38 +0300

    add get set

--- festival-repo/Festival/src/com/bondarovich/festival/entity/User.java
+++ festival-repo/Festival/src/com/bondarovich/festival/entity/User.java
@@ -2,6 +2,20 @@ package com.bondarovich.festival.entity;
 
 public class User {
 
-	String firstName;
-	String lastName;
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
 }
